4d0e42101b8d4d0739d45aef34bd631f64c11fa6
AlexKumar19 <alex.k.kumar@gmail.com>
2023-01-04 14:43:16 -0800

    updates

--- Discord.js
+++ Discord.js
@@ -1,5 +1,13 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -8,10 +16,10 @@ client.on('ready', () => {
 client.on('message', message => {
   // Split the message into individual words
   const words = message.content.split(' ');
-
+  const swearWords = ['damn', 'crap', 'heck', 'darn','fuck', 'shit'];
   // Check if any of the words are curse words
   const isSwearWord = words.some(word => swearWords.includes(word));
-
+  
   if (isSwearWord) {
     // Delete the message if it contains a curse word
     message.delete();
